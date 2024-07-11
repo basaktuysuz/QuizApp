b58219d6ae9c10e27396818b428113d9d4e9b48f
basaktuysuz1 <85128377+basaktuysuz@users.noreply.github.com>
2024-07-11 15:02:54 +0300

    ProfilePage updated

--- app/src/main/java/com/example/quizapp/ProfilePage.java
+++ app/src/main/java/com/example/quizapp/ProfilePage.java
@@ -1,14 +1,12 @@
 package com.example.quizapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,16 +19,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfilePage extends AppCompatActivity {
     FirebaseAuth mAuth;
-    TextView email , phoneNo,username,fullname;
+    TextView email, phoneNo, username, fullname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        email=findViewById(R.id.email_profile);
+        email = findViewById(R.id.email_profile);
 
-        username=findViewById(R.id.username_profile);
-        fullname=findViewById(R.id.fullname_profile);
+        username = findViewById(R.id.username_profile);
+        fullname = findViewById(R.id.fullname_profile);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -69,13 +68,13 @@ public class ProfilePage extends AppCompatActivity {
                             String urlFromDB = document.getString("URL");
                             String nameFromDB = document.getString("name");
                             String usernameFromDB = document.getString("username");
-                           // String phoneNoFromDB = document.getString("phoneNo");
+                            // String phoneNoFromDB = document.getString("phoneNo");
                             String emailFromDB = document.getString("email");
 
-email.setText(emailFromDB);
+                            email.setText(emailFromDB);
 //phoneNo.setText(phoneNoFromDB);
-username.setText(usernameFromDB);
-fullname.setText(nameFromDB);
+                            username.setText(usernameFromDB);
+                            fullname.setText(nameFromDB);
 
                         } else {
                             // user not exists
@@ -88,6 +87,7 @@ fullname.setText(nameFromDB);
                 }
             });
         }
-    }}
+    }
+}
 
 
--- app/src/main/java/com/example/quizapp/UserAdapter.java
+++ app/src/main/java/com/example/quizapp/UserAdapter.java
@@ -3,10 +3,12 @@ package com.example.quizapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.quizapp.R;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -32,10 +34,17 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         DocumentSnapshot document = userList.get(position);
         String userName = document.getString("name");
         Long userScore = document.getLong("score");
+        String url = document.getString("URL");
 
 
         holder.userNameTextView.setText(userName);
         holder.userScoreTextView.setText(userScore + " QP");
+        // Use Glide to load the image into the avatar ImageView
+        Glide.with(holder.avatar.getContext())
+                .load(url)
+                .placeholder(R.drawable.loading) // optional placeholder// optional error drawable
+                .into(holder.avatar);
+
 
         int listNumber = position + 4;
         holder.listNoTextview.setText(String.valueOf(listNumber));
@@ -43,19 +52,22 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     @Override
     public int getItemCount() {
-        return userList.size();
+        int limit = 6;
+        return Math.min(userList.size(), limit);
     }
 
     public static class UserViewHolder extends RecyclerView.ViewHolder {
         TextView userNameTextView;
         TextView userScoreTextView;
         TextView listNoTextview;
+        ImageView avatar;
 
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
             userNameTextView = itemView.findViewById(R.id.leaderName);
             userScoreTextView = itemView.findViewById(R.id.leaderScore);
             listNoTextview = itemView.findViewById(R.id.listNo);
+            avatar=itemView.findViewById(R.id.leaderAvatar);
         }
     }
 }
